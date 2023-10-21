505768491ca5089b22371c0548aa40b6467c22b1
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2023-10-21 09:56:40 +0200

    Fix

--- eLibrary/DataLayers/Context/eLibraryDbContext.cs
+++ eLibrary/DataLayers/Context/eLibraryDbContext.cs
@@ -14,7 +14,14 @@ namespace eLibrary.DataLayers.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=WINDOWS-4PGG12B; Initial Catalog=eLibrary;Integrated Security=True;TrustServerCertificate=True");
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(projectPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connectionString = configuration.GetConnectionString("eLibrary");
+
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
